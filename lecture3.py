ccdfa4240e719863891f1a88695bc8a0ac9968af
KennReinhart <kenfmr@gmail.com>
2026-02-02 01:41:59 +0700

    Lecture 3, exceptions

---
+++ lecture3.py
@@ -0,0 +1,21 @@
+# Exceptions
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            # Ask what number is X
+            x = int(input(prompt))
+        except ValueError:
+            # shows err if not INT entered
+            print("Integer only, try again")
+            # or use pass
+        else:
+            break
+    return x
+
+main()
+
+# and raise
