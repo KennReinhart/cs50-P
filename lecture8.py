78bf52a9d544eb6469fbb4a24970ec70f2c82d35
KennReinhart <kenfmr@gmail.com>
2026-02-17 18:13:45 +0700

    lecture 8, classes

--- lecture8.py
+++ lecture8.py
@@ -1,20 +1,22 @@
 # OOP, object oriented programming
 # tuples, another type of data thats a collection of values,similar to a list but its immutable(unable to be changed).
+class Student:
+    ...
 
 def main():
     # name, house = get_student()
     # print(f"{name} from {house}")
     student = get_student()
-    if student["name"].lower() == "padma":
-        student["name"] = "Padma"
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    # if student["name"].lower() == "padma":
+    #     student["name"] = "Padma"
+    #     student["house"] = "Ravenclaw"
+    print(f"{student.name} from {student.house}")
 
 def get_student():
     # using dict is less worrysome to remember which is 0,1 and so forth
-    student = {}
-    student["name"] = input("Name: ")
-    student["house"] = input("House: ")
+    # student = {}
+    # student["name"] = input("Name: ")
+    # student["house"] = input("House: ")
     # -- or --
     # name = input("Name: ")
     # house = input("House: ")
@@ -22,6 +24,11 @@ def get_student():
 
     # name = input("What is your name? ")
     # house = input("What is your house? ")
+
+    #using class
+    name = input("Enter your name: ")
+    house = input("Enter your house: ")
+    student = Student(name, house)
     return student #this right here, if [name, house] means lists
 
 if __name__ == "__main__":
