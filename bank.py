78544e51bbf7763896411978d6fdbc50b7b72f41
KennReinhart <kenfmr@gmail.com>
2026-02-25 17:46:54 +0700

    lecture 9, Docstring, Constant

--- bank.py
+++ bank.py
@@ -1,21 +1,62 @@
-#global
-balance = 0
+# #global
+# balance = 0
+#
+# def main():
+#     print("Balance:", balance)
+#     deposit = int(input("Enter deposit: "))
+#     depo(deposit)
+#     withdraw = int(input("Enter withdraw: "))
+#     cashout(withdraw)
+#     print("Balance:", balance)
+#
+# def depo(n):
+#     global balance
+#     balance += n
+#
+# def cashout(n):
+#     global balance
+#     balance -= n
+#
+# if __name__ == "__main__":
+#     main()
+
+#OOP
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+#constant
+class Cat:
+    MEOWS = 3
+
+    def meow(self):
+        for _ in range(Cat.MEOWS):
+            print("meow")
 
 def main():
-    print("Balance:", balance)
-    deposit = int(input("Enter deposit: "))
-    depo(deposit)
-    withdraw = int(input("Enter withdraw: "))
-    cashout(withdraw)
-    print("Balance:", balance)
+    account = Account()
+    print("Balance: ", account.balance)
+    account.deposit(100)
+    account.withdraw(25)
+    print("Balance: ", account.balance)
 
-def depo(n):
-    global balance
-    balance += n
+    cat = Cat()
+    cat.meow()
 
-def cashout(n):
-    global balance
-    balance -= n
-
+    """
+    Docstring, to explain things
+    :param n: number of times to n
+    :etc
+    """
 if __name__ == "__main__":
     main()
